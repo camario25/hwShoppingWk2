973f964eb1091926fb5d73d7fabfdd8b205e904d
camario25 <camario25@yahoo.com>
2018-02-03 08:30:48 -0800

    added click listener to buttons console works but no change in website

--- scripts/main.js
+++ scripts/main.js
@@ -4,16 +4,30 @@ var greensButton = document.getElementById("orgGreens");
 var essenButton = document.getElementById("essentials");
 var bevButton = document.getElementById("beverages");
 
+var greenPics = document.querySelector(".greenPics");
+var essentialPics = document.querySelector(".essentialPics");
+var drinkPics = document.querySelector(".drinkPics");
+
+
 greensButton.addEventListener('click', function (event){
-  
-})
+    drinkPics.display = "none";
+    essentialPics.display = "none";
+    greenPics.display = "block";
+    console.log("click")
+});
 essenButton.addEventListener('click', function (event){
-  
-})
+  drinkPics.display = "none";
+  greenPics.display = "none";
+  essentialPics.display = "flex";
+  console.log("click")
+});
 
 bevButton.addEventListener('click', function (event){
-  
-})
+  essentialPics.display = "none";
+  greenPics.display = "none";
+  drinkPics.display = "flex";
+  console.log("click")
+});
 
 // $( "#list" ).on( "click", "a", function( event ) {
 //     var elem = $( this );
