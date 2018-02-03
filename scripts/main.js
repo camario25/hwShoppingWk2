96382630135620a807a683a4fd9e48e5a494eefd
camario25 <camario25@yahoo.com>
2018-02-03 07:53:12 -0800

    starting js getting buttons

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,23 @@
 console.log('Hello');
+
+var greensButton = document.getElementById("orgGreens");
+var essenButton = document.getElementById("essentials");
+var bevButton = document.getElementById("beverages");
+
+greensButton.addEventListener('click', function (event){
+  
+})
+essenButton.addEventListener('click', function (event){
+  
+})
+
+bevButton.addEventListener('click', function (event){
+  
+})
+
+// $( "#list" ).on( "click", "a", function( event ) {
+//     var elem = $( this );
+//     if ( elem.is( "[href^='http']" ) ) {
+//         elem.attr( "target", "_blank" );
+//     }
+// });
