17dca95668c49ee2b369f624a7d585443989b181
camario25 <camario25@yahoo.com>
2018-02-03 09:43:56 -0800

    added some potential logic

--- scripts/main.js
+++ scripts/main.js
@@ -8,26 +8,26 @@ var greenPics = document.querySelector(".greenPics");
 var essentialPics = document.querySelector(".essentialPics");
 var drinkPics = document.querySelector(".drinkPics");
 
-
-greensButton.addEventListener('click', function (event){
+greensButton.addEventListener('click', function (){
     drinkPics.display = "none";
     essentialPics.display = "none";
-    greenPics.display = "block";
+    greenPics.display = "flex";
     console.log("click")
 });
-essenButton.addEventListener('click', function (event){
+essenButton.addEventListener('click', function (){
   drinkPics.display = "none";
   greenPics.display = "none";
   essentialPics.display = "flex";
   console.log("click")
 });
 
-bevButton.addEventListener('click', function (event){
+bevButton.addEventListener('click', function (){
   essentialPics.display = "none";
   greenPics.display = "none";
   drinkPics.display = "flex";
   console.log("click")
 });
+
 
 // $( "#list" ).on( "click", "a", function( event ) {
 //     var elem = $( this );
@@ -46,14 +46,58 @@ var topRight = document.querySelector(".topRight");
 var bottomLeft = document.querySelector(".bottomLeft");
 var bottomRight =document.querySelector(".topRight");
 
+function pushToShoppingArray (){
+  topLeft.addEventListener('click', function () {
 if (greenPics.display = flex) {
   
-}
+};
 
 if (essentialPics.display = flex) {
   
-}
+};
 
 if (drinkPics.display = flex) {
   
-}
+  };
+};
+topRight.addEventListener('click', function () {
+  if (greenPics.display = flex) {
+    
+  };
+
+  if (essentialPics.display = flex) {
+    
+  };
+
+  if (drinkPics.display = flex) {
+    
+  };
+};
+bottomLeft.addEventListener('click', function () {
+  if (greenPics.display = flex) {
+    
+  };
+
+  if (essentialPics.display = flex) {
+    
+  };
+
+  if (drinkPics.display = flex) {
+    
+  };
+};
+bottomRight.addEventListener('click', function () {
+  if (greenPics.display = flex) {
+    
+  };
+
+  if (essentialPics.display = flex) {
+    
+  };
+
+  if (drinkPics.display = flex) {
+    
+  };
+};
+};
+
